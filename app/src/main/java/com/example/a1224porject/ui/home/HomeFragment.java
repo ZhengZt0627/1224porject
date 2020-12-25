a82526c6cdb616f67b81f49bf3a942fefa0652c7
ZhengZt0627 <2654107259@qq.com>
2020-12-25 21:54:16 +0800

    周一周四新品

--- app/src/main/java/com/example/a1224porject/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/a1224porject/ui/home/HomeFragment.java
@@ -22,7 +22,10 @@ import com.alibaba.android.vlayout.VirtualLayoutManager;
 import com.alibaba.android.vlayout.layout.GridLayoutHelper;
 import com.alibaba.android.vlayout.layout.SingleLayoutHelper;
 import com.example.a1224porject.R;
+import com.example.a1224porject.adapter.BannerAdapter;
+import com.example.a1224porject.adapter.FindAdapter;
 import com.example.a1224porject.adapter.MainSingleAdapter;
+import com.example.a1224porject.adapter.MonAdapter;
 import com.example.a1224porject.adapter.NextSingleAdapter;
 import com.example.a1224porject.adapter.TextAdapter;
 import com.example.a1224porject.base.BaseFragment;
@@ -40,37 +43,49 @@ public class HomeFragment extends BaseFragment<MainSinglePresenter> implements M
     private RecyclerView rlv;
     private VirtualLayoutManager vmanager;
     private ViewPager vp;
-    private int images[] ={R.mipmap.p1,R.mipmap.p2,R.mipmap.p3};
-    private List<View> views=new ArrayList<>();
+    private List<View> views = new ArrayList<>();
     private ArrayList<MainSingleBean.DataDTO.ChannelDTO> singlelist;
     private MainSingleAdapter mainSingleAdapter;
     private ArrayList<MainSingleBean.DataDTO.BrandListDTO> nextlist;
     private NextSingleAdapter nextSingleAdapter;
     private TextAdapter textAdapter;
+    private ArrayList<MainSingleBean.DataDTO.BannerDTO> bannerlist;
+    private BannerAdapter bannerAdapter;
+    private FindAdapter findAdapter;
+    private MonAdapter monAdapter;
 
 
     protected void initView(View view) {
         rlv = view.findViewById(R.id.rlv);
-        vp = view.findViewById(R.id.vp);
-
-        //将images数组中的图片放入ImageView
-        for (int i = 0; i < images.length; i++) {
-            ImageView imageView=new ImageView(getActivity());
-            imageView.setImageResource(images[i]);
-            views.add(imageView);
-        }
-        vp.setAdapter(new MyAdapter());
 
         //创建Vlayout对象
         vmanager = new VirtualLayoutManager(getActivity());
 
         RecyclerView.RecycledViewPool pool = new RecyclerView.RecycledViewPool();
         rlv.setRecycledViewPool(pool);
-        pool.setMaxRecycledViews(0,20);
+        pool.setMaxRecycledViews(0, 20);
 //设置适配器1
         DelegateAdapter delegateAdapter = new DelegateAdapter(vmanager);
 
+        //搜索框
+        //定义文字
+        SingleLayoutHelper singleLayoutHelper2 = new SingleLayoutHelper();
+        // 公共属性
+        singleLayoutHelper2.setMargin(10, 10, 10, 10);// 设置LayoutHelper边缘相对父控件（即RecyclerView）的距离
+        singleLayoutHelper2.setAspectRatio(4);// 设置设置布局内每行布局的宽与高的比
+        singleLayoutHelper2.setItemCount(1);// 设置布局里Item个数
+        findAdapter = new FindAdapter(singleLayoutHelper2, getActivity());
+        delegateAdapter.addAdapter(findAdapter);
 
+//第一个banner
+        bannerlist = new ArrayList<>();
+        //定义文字
+        SingleLayoutHelper singleLayoutHelper1 = new SingleLayoutHelper();
+        // 公共属性
+        singleLayoutHelper1.setItemCount(1);// 设置布局里Item个数
+
+        bannerAdapter = new BannerAdapter(bannerlist,getActivity(),singleLayoutHelper1);
+        delegateAdapter.addAdapter(bannerAdapter);
         /**
          设置Grid布局
          */
@@ -89,7 +104,7 @@ public class HomeFragment extends BaseFragment<MainSinglePresenter> implements M
         gridLayoutHelper.setHGap(20);// 控制子元素之间的水平间距
         gridLayoutHelper.setAutoExpand(false);//是否自动填充空白区域
         gridLayoutHelper.setSpanCount(5);// 设置每行多少个网格
-        mainSingleAdapter = new MainSingleAdapter(gridLayoutHelper,getActivity(),singlelist);
+        mainSingleAdapter = new MainSingleAdapter(gridLayoutHelper, getActivity(), singlelist);
         //设置适配器2
         delegateAdapter.addAdapter(mainSingleAdapter);
 
@@ -98,19 +113,15 @@ public class HomeFragment extends BaseFragment<MainSinglePresenter> implements M
         SingleLayoutHelper singleLayoutHelper = new SingleLayoutHelper();
         // 公共属性
         singleLayoutHelper.setItemCount(1);// 设置布局里Item个数
-//        singleLayoutHelper.setPadding(10, 10, 10, 10);// 设置LayoutHelper的子元素相对LayoutHelper边缘的距离
-//        singleLayoutHelper.setMargin(10, 10, 10, 10);// 设置LayoutHelper边缘相对父控件（即RecyclerView）的距离
-        singleLayoutHelper.setBgColor(Color.WHITE);// 设置背景颜色
-        singleLayoutHelper.setAspectRatio(4);// 设置设置布局内每行布局的宽与高的比
-        textAdapter = new TextAdapter(singleLayoutHelper,getActivity());
-       delegateAdapter.addAdapter(textAdapter);
+        textAdapter = new TextAdapter(singleLayoutHelper, getActivity());
+        delegateAdapter.addAdapter(textAdapter);
 
 
         //列表展示：
         nextlist = new ArrayList<>();
         ///< 网格布局 - 构造中传入相应的列的数量
         GridLayoutHelper gridLayoutHelper1 = new GridLayoutHelper(4);
-      //gridLayoutHelper1.setMarginTop(30);
+        //gridLayoutHelper1.setMarginTop(30);
         gridLayoutHelper1.setSpanCount(2);
         //设置垂直方向条目的间隔
         gridLayoutHelper1.setVGap(5);
@@ -118,12 +129,20 @@ public class HomeFragment extends BaseFragment<MainSinglePresenter> implements M
         gridLayoutHelper1.setHGap(5);
         gridLayoutHelper1.setMarginLeft(30);
         gridLayoutHelper1.setMarginRight(30);
-      //  gridLayoutHelper1.setMarginBottom(30);
+        //  gridLayoutHelper1.setMarginBottom(30);
         //自动填充满布局，在设置完权重，若没有占满，自动填充满布局
         gridLayoutHelper1.setAutoExpand(true);
         nextSingleAdapter = new NextSingleAdapter(nextlist, gridLayoutHelper1, getActivity());
         //设置适配器2
         delegateAdapter.addAdapter(nextSingleAdapter);
+
+        //固定文字
+        //定义文字
+        SingleLayoutHelper singleLayoutHelper4 = new SingleLayoutHelper();
+        // 公共属性
+        singleLayoutHelper4.setItemCount(1);// 设置布局里Item个数
+        monAdapter = new MonAdapter(singleLayoutHelper4, getActivity());
+        delegateAdapter.addAdapter(monAdapter);
 
         //放最后
         rlv.setAdapter(delegateAdapter);
@@ -151,8 +170,12 @@ public class HomeFragment extends BaseFragment<MainSinglePresenter> implements M
     public void onScuess(MainSingleBean mainSingleBean) {
         singlelist.addAll(mainSingleBean.getData().getChannel());
         nextlist.addAll(mainSingleBean.getData().getBrandList());
+        bannerlist.addAll(mainSingleBean.getData().getBanner());
+        bannerAdapter.notifyDataSetChanged();
         nextSingleAdapter.notifyDataSetChanged();
         mainSingleAdapter.notifyDataSetChanged();
+        findAdapter.notifyDataSetChanged();
+        monAdapter.notifyDataSetChanged();
     }
 
     @Override
@@ -160,28 +183,4 @@ public class HomeFragment extends BaseFragment<MainSinglePresenter> implements M
 
     }
 
-    //图片
-    class MyAdapter extends PagerAdapter{
-
-        @Override
-        public int getCount() {
-            return views.size();
-        }
-        @Override
-        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-            return view==object;
-        }
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            View v=views.get(position);
-            container.addView(v);
-            return v;
-        }
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            View v=views.get(position);
-            //前一张图片划过后删除该View
-            container.removeView(v);
-        }
-    }
 }
