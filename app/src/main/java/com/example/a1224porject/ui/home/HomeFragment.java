ca54c1f471e5c7b2ed7bc480c0309948d4441dc1
ZhengZt0627 <2654107259@qq.com>
2020-12-25 12:00:24 +0800

    准备使用Vlayout实现横向滑动

--- app/src/main/java/com/example/a1224porject/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/a1224porject/ui/home/HomeFragment.java
@@ -11,17 +11,31 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.alibaba.android.vlayout.VirtualLayoutManager;
 import com.example.a1224porject.R;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private RecyclerView rlv;
+    private VirtualLayoutManager vmanager;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
       //  homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        initView(view);
        return view;
+    }
 
+    private void initView(View view) {
+        rlv = view.findViewById(R.id.rlv);
+        //创建Vlayout对象
+        vmanager = new VirtualLayoutManager(getActivity());
+
+        RecyclerView.RecycledViewPool pool = new RecyclerView.RecycledViewPool();
+        rlv.setRecycledViewPool(pool);
+        pool.setMaxRecycledViews(0,100);
     }
 }
