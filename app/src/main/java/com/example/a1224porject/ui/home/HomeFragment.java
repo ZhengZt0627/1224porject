631f4b408d928ddaec2be0f9ae7e6ff2f17d1f0b
ZhengZt0627 <2654107259@qq.com>
2020-12-27 10:25:49 +0800

    人气推荐标题网格完成

--- app/src/main/java/com/example/a1224porject/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/a1224porject/ui/home/HomeFragment.java
@@ -24,6 +24,7 @@ import com.alibaba.android.vlayout.layout.SingleLayoutHelper;
 import com.example.a1224porject.R;
 import com.example.a1224porject.adapter.BannerAdapter;
 import com.example.a1224porject.adapter.FindAdapter;
+import com.example.a1224porject.adapter.HotGoodsAdapter;
 import com.example.a1224porject.adapter.MainSingleAdapter;
 import com.example.a1224porject.adapter.MonAdapter;
 import com.example.a1224porject.adapter.NewGoodsAdapter;
@@ -58,6 +59,9 @@ public class HomeFragment extends BaseFragment<MainSinglePresenter> implements M
     private ArrayList<MainSingleBean.DataDTO.NewGoodsListDTO> goodslist;
     private NewGoodsAdapter newGoodsAdapter;
     private com.example.a1224porject.adapter.ploAdapter ploAdapter;
+    private ArrayList<MainSingleBean.DataDTO.HotGoodsListDTO> hotlist;
+    private DelegateAdapter delegateAdapter;
+    private HotGoodsAdapter hotGoodsAdapter;
 
 
     protected void initView(View view) {
@@ -69,11 +73,25 @@ public class HomeFragment extends BaseFragment<MainSinglePresenter> implements M
         RecyclerView.RecycledViewPool pool = new RecyclerView.RecycledViewPool();
         rlv.setRecycledViewPool(pool);
         pool.setMaxRecycledViews(0, 20);
-//设置适配器1
-        DelegateAdapter delegateAdapter = new DelegateAdapter(vmanager);
-
+        //设置总体适配器
+        delegateAdapter = new DelegateAdapter(vmanager);
         //搜索框
-        //定义文字
+        initSearch();
+        //banner
+        initBanner();
+        //专题行
+        initZhuanti();
+        //对应的文字和网格
+        initNext();
+        //放最后
+        rlv.setAdapter(delegateAdapter);
+        rlv.setLayoutManager(vmanager);
+
+    }
+
+
+
+    private void initSearch() {
         SingleLayoutHelper singleLayoutHelper2 = new SingleLayoutHelper();
         // 公共属性
         singleLayoutHelper2.setMargin(10, 10, 10, 10);// 设置LayoutHelper边缘相对父控件（即RecyclerView）的距离
@@ -82,7 +100,8 @@ public class HomeFragment extends BaseFragment<MainSinglePresenter> implements M
         findAdapter = new FindAdapter(singleLayoutHelper2, getActivity());
         delegateAdapter.addAdapter(findAdapter);
 
-//第一个banner
+    }
+    private void initBanner() {
         bannerlist = new ArrayList<>();
         //定义文字
         SingleLayoutHelper singleLayoutHelper1 = new SingleLayoutHelper();
@@ -91,9 +110,10 @@ public class HomeFragment extends BaseFragment<MainSinglePresenter> implements M
 
         bannerAdapter = new BannerAdapter(bannerlist,getActivity(),singleLayoutHelper1);
         delegateAdapter.addAdapter(bannerAdapter);
-        /**
-         设置Grid布局
-         */
+    }
+
+    private void initZhuanti() {
+
         singlelist = new ArrayList<>();
         GridLayoutHelper gridLayoutHelper = new GridLayoutHelper(5);
         // 在构造函数设置每行的网格个数
@@ -113,14 +133,16 @@ public class HomeFragment extends BaseFragment<MainSinglePresenter> implements M
         //设置适配器2
         delegateAdapter.addAdapter(mainSingleAdapter);
 
+    }
+
+    private void initNext() {
         //定义文字
         SingleLayoutHelper singleLayoutHelper = new SingleLayoutHelper();
         // 公共属性
         singleLayoutHelper.setItemCount(1);// 设置布局里Item个数
         textAdapter = new TextAdapter(singleLayoutHelper, getActivity());
         delegateAdapter.addAdapter(textAdapter);
-
-        //列表展示：
+        //直供
         nextlist = new ArrayList<>();
         ///< 网格布局 - 构造中传入相应的列的数量
         GridLayoutHelper gridLayoutHelper1 = new GridLayoutHelper(4);
@@ -138,15 +160,12 @@ public class HomeFragment extends BaseFragment<MainSinglePresenter> implements M
         nextSingleAdapter = new NextSingleAdapter(nextlist, gridLayoutHelper1, getActivity());
         //设置适配器2
         delegateAdapter.addAdapter(nextSingleAdapter);
-
-        //固定文字
         //定义文字
         SingleLayoutHelper singleLayoutHelper4 = new SingleLayoutHelper();
         // 公共属性
         singleLayoutHelper4.setItemCount(1);// 设置布局里Item个数
         monAdapter = new MonAdapter(singleLayoutHelper4, getActivity());
         delegateAdapter.addAdapter(monAdapter);
-
         //周一周日网格：
         goodslist = new ArrayList<>();
         ///< 网格布局 - 构造中传入相应的列的数量
@@ -164,8 +183,6 @@ public class HomeFragment extends BaseFragment<MainSinglePresenter> implements M
         gridLayoutHelper2.setAutoExpand(true);
         newGoodsAdapter = new NewGoodsAdapter(goodslist, gridLayoutHelper2, getActivity());
         delegateAdapter.addAdapter(newGoodsAdapter);
-
-        //固定文字
         //定义文字：人气推荐
         SingleLayoutHelper singleLayoutHelper5 = new SingleLayoutHelper();
         // 公共属性
@@ -173,14 +190,12 @@ public class HomeFragment extends BaseFragment<MainSinglePresenter> implements M
         ploAdapter = new ploAdapter(singleLayoutHelper5,getActivity());
         delegateAdapter.addAdapter(ploAdapter);
         //人气推荐网格：
-
-
-
-
-
-        //放最后
-        rlv.setAdapter(delegateAdapter);
-        rlv.setLayoutManager(vmanager);
+        hotlist = new ArrayList<>();
+        GridLayoutHelper gridLayoutHelper3 = new GridLayoutHelper(3);
+        gridLayoutHelper3.setSpanCount(1);
+        gridLayoutHelper3.setItemCount(4);
+        hotGoodsAdapter = new HotGoodsAdapter(gridLayoutHelper3,getActivity(),hotlist);
+        delegateAdapter.addAdapter(hotGoodsAdapter);
 
     }
 
@@ -206,6 +221,7 @@ public class HomeFragment extends BaseFragment<MainSinglePresenter> implements M
         nextlist.addAll(mainSingleBean.getData().getBrandList());
         bannerlist.addAll(mainSingleBean.getData().getBanner());
         goodslist.addAll(mainSingleBean.getData().getNewGoodsList());
+        hotlist.addAll(mainSingleBean.getData().getHotGoodsList());
         bannerAdapter.notifyDataSetChanged();
         nextSingleAdapter.notifyDataSetChanged();
         mainSingleAdapter.notifyDataSetChanged();
@@ -213,6 +229,7 @@ public class HomeFragment extends BaseFragment<MainSinglePresenter> implements M
         monAdapter.notifyDataSetChanged();
         newGoodsAdapter.notifyDataSetChanged();
         ploAdapter.notifyDataSetChanged();
+        hotGoodsAdapter.notifyDataSetChanged();
     }
 
     @Override
